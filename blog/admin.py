427f62909d38038704204e4ad4f1fe86b9b0c826
Mukut Brahma <mukutb.mac@gmail.com>
2022-04-16 15:05:18 +0530

    created models and migrations

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Author, Post, Tag
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'date']
+    list_filter = ['date']
+    search_fields = ['title', 'content']
+    readonly_fields = ['slug']
+
+
+admin.site.register(Author)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,40 @@
+from pyexpat import model
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=200)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(default='', db_index=True)
+
+    tags = models.ManyToManyField(Tag, related_name='posts')
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, null=True, related_name='posts')
+
+    def __str__(self) -> str:
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
